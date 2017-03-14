64f74be7ab62e9d63258bcf90044c8867cad0bfd
chandanchanda66 <chandanchanda66@gmail.com>
2017-03-14 23:29:52 +0530

    Init commit

--- src/javaapplication6/addingclients.java
+++ src/javaapplication6/addingclients.java
@@ -5,6 +5,16 @@
  */
 package javaapplication6;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author chandan
@@ -39,12 +49,13 @@ public class addingclients extends javax.swing.JFrame {
         jTextField3 = new javax.swing.JTextField();
         jPasswordField1 = new javax.swing.JPasswordField();
         jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new java.awt.BorderLayout(200, 150));
 
         java.awt.GridBagLayout jPanel1Layout = new java.awt.GridBagLayout();
-        jPanel1Layout.columnWidths = new int[] {150};
+        jPanel1Layout.columnWidths = new int[] {200};
         jPanel1.setLayout(jPanel1Layout);
 
         jLabel1.setText("USERNAME");
@@ -96,6 +107,15 @@ public class addingclients extends javax.swing.JFrame {
         gridBagConstraints.ipadx = 100;
         gridBagConstraints.insets = new java.awt.Insets(0, 0, 20, 0);
         jPanel1.add(jTextField2, gridBagConstraints);
+
+        jTextField3.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyPressed(java.awt.event.KeyEvent evt) {
+                jTextField3KeyPressed(evt);
+            }
+            public void keyReleased(java.awt.event.KeyEvent evt) {
+                jTextField3KeyReleased(evt);
+            }
+        });
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 1;
         gridBagConstraints.gridy = 3;
@@ -108,12 +128,33 @@ public class addingclients extends javax.swing.JFrame {
         gridBagConstraints.insets = new java.awt.Insets(0, 0, 20, 0);
         jPanel1.add(jPasswordField1, gridBagConstraints);
 
+        jButton1.setForeground(new java.awt.Color(0, 51, 204));
         jButton1.setText("GO");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
         gridBagConstraints = new java.awt.GridBagConstraints();
-        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridx = 0;
         gridBagConstraints.gridy = 4;
         gridBagConstraints.insets = new java.awt.Insets(20, 0, 20, 0);
         jPanel1.add(jButton1, gridBagConstraints);
+
+        jButton2.setForeground(new java.awt.Color(0, 51, 204));
+        jButton2.setText("Back");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 4;
+        gridBagConstraints.gridwidth = 10;
+        gridBagConstraints.gridheight = 10;
+        gridBagConstraints.ipadx = 1;
+        jPanel1.add(jButton2, gridBagConstraints);
 
         getContentPane().add(jPanel1, java.awt.BorderLayout.CENTER);
 
@@ -123,6 +164,55 @@ public class addingclients extends javax.swing.JFrame {
     private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_jTextField1ActionPerformed
+String temp;
+    private void jTextField3KeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jTextField3KeyPressed
+
+        if((evt.getKeyCode()>=96)&&(evt.getKeyCode()<=105) || evt.getKeyCode() == 8)
+       {
+           temp = null;
+       }
+       else
+       {
+           temp = jTextField3.getText();
+       }        // TODO add your handling code here:
+    }//GEN-LAST:event_jTextField3KeyPressed
+
+    private void jTextField3KeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jTextField3KeyReleased
+ if(temp != null)
+       {
+           jTextField3.setText(temp);
+       }
+       else
+       {
+           //jTextField1.setText("hi");
+       }        // TODO add your handling code here:
+    }//GEN-LAST:event_jTextField3KeyReleased
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+       
+                new Adminview().setVisible(true);
+    }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+      try {
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/billing", "root", "pass");
+            String sql = "INSERT INTO loginusers(username,password,email,phone) values (?,?,?,?)";
+PreparedStatement statement = conn.prepareStatement(sql);
+statement.setString(1,jTextField1.getText());
+statement.setString(2,jPasswordField1.getText());
+statement.setString(3,jTextField2.getText());
+statement.setString(4,jTextField3.getText());
+statement.executeUpdate();
+Mailer.send("chandanchanda66@gmail.com","chandan-2409",jTextField2.getText(),"Client Added","Welcome To abc Group Of Company"+
+        "\nYour UserName"+jTextField1.getText()+"Password"+jPasswordField1.getText());  
+ JOptionPane.showMessageDialog(null,"Client Added"); 
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null,ex); 
+        }
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -161,6 +251,7 @@ public class addingclients extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
